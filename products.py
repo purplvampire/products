8f5a1e2b440cc553156d1de8834787a245abf65e
Jason Chen <purplvampire@gmail.com>
2019-03-24 16:58:34 +0800

    add header and encoding

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
     if name == 'q':
         break
     price = input('請輸入商品價格: ')
+    price = int(price)
     # 第一種作法
     # p = []
     # p.append(name)
@@ -27,4 +28,11 @@ for product in products:
     print(product)
 # 改成名稱對應價格
     print('商品', product[0], '的價格是', product[1], '元')
+
+# 將資料儲存
+with open('products.csv', 'w', encoding='utf-8') as f:    # 建立檔案並給予寫入(w)權限,設定編碼為通用模式(utf-8)
+    f.write('商品,價格\n')      # 先加入欄位名稱
+    for product in products:   # 取出清單中每筆資料
+        f.write(product[0] + ',' + str(product[1]) + '\n')   # 將清單中每筆資料個別放入每行(\n)中,用+串聯字串與逗號(Excel分欄位用)
+
 
